b9329ab99d8d87481f4600e6a7ae6305085de5e4
alumno <alumno@Alumno14>
2022-02-02 11:26:46 +0100

    commit

---
+++ ValidationCardDocker/src/test/java/com/ejemplos/spring/cucumber/CucumberSpringConfiguration.java
@@ -0,0 +1,16 @@
+package com.ejemplos.spring.cucumber;
+
+import org.springframework.boot.test.context.SpringBootContextLoader;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.ejemplos.spring.ValidationCardDockerApplication;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+
+@CucumberContextConfiguration
+@SpringBootTest(classes = ValidationCardDockerApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ContextConfiguration(classes = ValidationCardDockerApplication.class, loader = SpringBootContextLoader.class)
+public class CucumberSpringConfiguration {
+	// implementation executePost
+}
