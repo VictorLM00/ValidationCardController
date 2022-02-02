833e392fb53ab987ac8c3cdbb5fa30430dc67fee
alumno <alumno@Alumno14>
2022-02-02 11:06:18 +0100

    pruebas

--- ValidationCardDocker/src/main/java/com/ejemplos/spring/controller/CardController.java
+++ ValidationCardDocker/src/main/java/com/ejemplos/spring/controller/CardController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ejemplos.spring.model.Card;
@@ -28,7 +29,7 @@ public class CardController {
 	
 	@PostMapping
 	public String saveRegistration(
-							@Valid Card card,
+							@RequestBody Card card,
 							BindingResult result, 
 							ModelMap model) {
 
--- ValidationCardDocker/src/test/java/com/ejemplos/spring/cucumber/CucumberIntegrationTest.java
+++ ValidationCardDocker/src/test/java/com/ejemplos/spring/cucumber/CucumberIntegrationTest.java
@@ -16,24 +16,67 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import net.minidev.json.JSONObject;
 
 @RunWith(Cucumber.class) // ejecutar pruebas de Cucumber con JUnit
 @CucumberOptions(features = "src/main/resources") // especificamos la ubicación del archivo Gherkin
 public class CucumberIntegrationTest {
 
 	@When("^el cliente llama a /")
-	public void the_client_issues_POST_version() throws Throwable {
-		// executeGet("http://localhost:8888/version");
+	public void the_client_call_POST() throws Throwable {
+		// executeGet("http://localhost:8080/");
 	}
 
 	@Then("^el cliente resive un mensage de success")
 	public void the_client_receives_message_success(@Valid Card card, BindingResult result, ModelMap model)
 			throws Throwable {
 		
+		JSONObject request = new JSONObject();
+
+		request.put("num1", "3242");
+		request.put("num2", "3242");
+		request.put("num3", "3242");
+		request.put("num4", "3242");
+		request.put("month", "3");
+		request.put("year", "23");
+
+		System.out.println(request.toJSONString());
+
+		RestAssured.baseURI = "http://localhost:8080";
+
+		RestAssured.given().header("Content-Type", "aplication/json").contentType(ContentType.JSON)
+				.accept(ContentType.JSON).body(request.toJSONString()).when().post("/").then().statusCode(201)
+				.log().all();
+		
 	}
 
 	@And("^el cliente resive una pagina con un mesage de compra realizada")
 	public void the_client_receives_web_message(String web) throws Throwable {
 		// assertThat(latestResponse.getBody(), is(version));
+	}
+	
+	@Then("^el cliente resive un mensage de success")
+	public void the_client_receives_message_error(@Valid Card card, BindingResult result, ModelMap model)
+			throws Throwable {
+		
+		JSONObject request = new JSONObject();
+
+		request.put("num1", "32425");
+		request.put("num2", "3242");
+		request.put("num3", "3242");
+		request.put("num4", "3242");
+		request.put("month", "1");
+		request.put("year", "20");
+
+		System.out.println(request.toJSONString());
+
+		RestAssured.baseURI = "http://localhost:8080";
+
+		RestAssured.given().header("Content-Type", "aplication/json").contentType(ContentType.JSON)
+				.accept(ContentType.JSON).body(request.toJSONString()).when().post("/").then().statusCode(400)
+				.log().all();
+		
 	}
 }
